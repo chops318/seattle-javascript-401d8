b5850d9454260655ebb639ce8768f195364fb741
toastynerd <tyler@codefellows.com>
2016-09-06 16:00:31 -0700

    end of day

--- week_9/sep_6_client_auth/app/components/index.js
+++ week_9/sep_6_client_auth/app/components/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 module.exports = (app) => {
+  require('./sign_up')(app);
+  require('./sign_in')(app);
   require('./list-form')(app);
   require('./list')(app);
 };
--- week_9/sep_6_client_auth/app/entry.js
+++ week_9/sep_6_client_auth/app/entry.js
@@ -4,10 +4,14 @@ require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/base.scss');
 
 const angular = require('angular');
-let demoApp = angular.module('demoApp', []);
+let demoApp = angular.module('demoApp', [require('angular-route')]);
+
+require('./controller')(demoApp);
+require('./components')(demoApp);
 
 demoApp.run(['$rootScope', ($rs) => {
-  $rs.noteListUrl = `${__API_URL__}/api/list`;
+  $rs.baseUrl = `${__API_URL__}`;
+  $rs.noteListUrl = $rs.baseUrl + '/api/list';
   $rs.noteHttpConfig = {
     headers: {
       'Content-Type': 'application/json',
@@ -16,4 +20,18 @@ demoApp.run(['$rootScope', ($rs) => {
   };
 }]);
 
-require('./components')(demoApp);
+demoApp.config(['$routeProvider', ($rp) => {
+  $rp
+    .when('/notes', {
+      template: require('./html/notes.html')
+    })
+    .when('/signup', {
+      template: require('./html/signup.html')
+    })
+    .when('/signin', {
+      template: require('./html/signin.html')
+    })
+    .otherwise({
+      redirectTo: 'notes'
+    });
+}]);
